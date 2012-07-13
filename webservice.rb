28c0ee8d1db6d0a91c10f9fd4161d05a59704c26
inve1 <inventore1@gmail.com>
2012-07-13 00:38:45 -0400

    temporary fix to queries, can add users, receives messages properly

--- webservice.rb
+++ webservice.rb
@@ -32,40 +32,62 @@ EventMachine.run do
 
 
         get '/messages/:number' do |num|
-            resp = @@couch['messages/_design/query/_view/mess_fromto'].get 'key' => "%22#{num}%22"
-            puts resp.to_str
+            resp = @@couch["messages/_design/query/_view/mess_fromto?key=%22#{num}%22"].get
             mess = JSON.parse(resp)['rows']
             return mess.to_json
         end
         
-        def self.add_message(number, text)
-            tosend = { 'fromto' => number, 'is_sent' => true, 'date' => Time.now.to_i, 'text' => text }
+        def self.add_message(number, text, is_sent)
+            tosend = { 'fromto' => number, 'is_sent' => is_sent, 'date' => Time.now.to_i, 'text' => text }
             @@couch['/messages/'].post tosend.to_json , :content_type => :json, :accept => :json
-            @@channel.push({'value' => tosend}.to_json)
+            @@channel.push({'message' => {'value' => tosend}}.to_json)
         end
 
         post %r{/messages/(\w+)} do |num|
-            Tabellarius.add_message(num, params[:text])
+            Tabellarius.add_message(num, params[:text], true)
         end
 
         post %r{/messages/\+(\d+)} do |num|
-            users = @@couch['users/_design/query/_view/number_id/'].get 'key' => "%22#{num}%22"
-            us = JSON.parse(users)['rows'][0]['value']
-            Tabellarius.add_message(us, params[:text])
+            users = @@couch["users/_design/query/_view/number_id?key=%22#{num}%22"].get
+
+            parsed =  JSON.parse(users)
+            if parsed['rows'] == []
+                id = Tabellarius.add_user('Unknown', num)
+            else
+                id = parsed['rows'][0]['value']
+            end
+            Tabellarius.add_message(id, params[:text], false)
+            return ""
         end
+
+       def self.add_user(name, num)
+            new_user = { 'name' => name, 'number' => num }
+            resp = @@couch['/users/'].post new_user.to_json , :content_type => :json, :accept => :json
+            parsed_resp = JSON.parse(resp)
+            to_send = {'user' => { 'key' => parsed_resp['id'], 'value' => new_user}}
+            @@channel.push(to_send.to_json)
+            return parsed_resp['id']
+        end
+
+        post %r{/users/(\w+)/\+(\w+)} do |name, num|
+            Tabellarius.add_user(name, num)
+            return ""
+        end
+
+
     end
 
 
     EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
         ws.onopen {
             sid = Tabellarius.channel.subscribe { |msg| ws.send msg }
-            puts 'asd'
 
             ws.onclose { Tabellarius.channel.unsubscribe(sid) }
             ws.onmessage { |msg|
+                puts msg
                 mess = JSON.parse(msg)
                 num = mess['number']
-                Tabellarius.add_message(num, mess['text'])
+                Tabellarius.add_message(num, mess['text'], true)
             }
         }
     end
